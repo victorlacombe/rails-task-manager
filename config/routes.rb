11602a6a24ec3bbe9ff132f22bfe9f92fa4bb6d7
Victor Lacombe <lacombe.victor@hotmail.fr>
2018-05-15 16:26:57 +0200

    added methods to create, add, edit, destroy task

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Read all tasks
+  get "/tasks", to: "tasks#index"
+
+  # Create one task
+  get "tasks/new", to: "tasks#new", as: "new"
+  post "tasks/", to: "tasks#create", as: "create"
+
+  # Read one task
+  get "/tasks/:id", to: "tasks#show", as: "task"
+
+  # Update one task
+  get "tasks/:id/edit", to: "tasks#edit", as: "edit"
+  patch "tasks/:id", to: "tasks#update"
+
+  # Delete one task
+  delete "tasks/:id", to: "tasks#destroy", as: "delete"
+
 end
